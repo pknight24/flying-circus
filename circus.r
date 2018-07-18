ac0a83bb245f8470fe8a04c7bedac217d5afcdc1
Parker Knight <pknight24@protonmail.com>
2018-07-17 22:44:22 -0400

    Adds word vectors

--- circus.r
+++ circus.r
@@ -1,6 +1,7 @@
 library(dplyr)
 library(markovchain)
 library(RSQLite)
+library(purrr)
 source("episode.r")
 
 db <- dbConnect(SQLite(), dbname="database.sqlite")
@@ -8,7 +9,11 @@ db <- dbConnect(SQLite(), dbname="database.sqlite")
 q <- dbSendQuery(db, "SELECT * FROM scripts")
 
 data <- dbFetch(q)
-pretty.print(data, s = "1/1")
+data <- mutate(data, speaker = ifelse(is.na(character), "Background", "Character"))
+
+speakers <- data$speaker
+character.words <- filter(data, speaker == "Character")$detail %>% paste(collapse=" ") %>% strsplit(" ") %>% map(tolower)
+background.words <- filter(data, speaker == "Background")$detail %>% paste(collapse=" ") %>% strsplit(" ") %>% map(tolower)
 
 dbClearResult(q)
 dbDisconnect(db)
